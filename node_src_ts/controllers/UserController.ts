da9e2b4424a0b75235b22324c4ad6b877cf734a5
Chau Tran <ctch5@mail.umsl.edu>
2018-09-14 09:59:04 -0500

    Changed UserController to use try/catch

--- node_src_ts/controllers/UserController.ts
+++ node_src_ts/controllers/UserController.ts
@@ -3,7 +3,6 @@ import { IUserRepository } from '../repositories/IUserRepository';
 import { UserRepository } from '../repositories/UserRepository';
 import { User, UserModel } from '../models/User';
 import { Request, Response } from 'express';
-import { MongoError } from 'mongodb';
 import { sign } from 'jsonwebtoken';
 import { compare } from 'bcryptjs';
 import { Model } from 'mongoose';
@@ -23,14 +22,14 @@ export class UserController extends BaseController<User> {
     this._userRepository = new UserRepository(model);
     this.s3 = new S3({
       accessKeyId: coreConfig.aws.accessKey,
-      secretAccessKey: coreConfig.aws.secretKey
+      secretAccessKey: coreConfig.aws.secretKey,
     });
   }
 
   private deleteProfilePic(imageKey: string) {
     const params: DeleteObjectRequest = {
       Bucket: coreConfig.aws.bucketName,
-      Key: imageKey
+      Key: imageKey,
     };
 
     this.s3.deleteObject(params, (err: AWSError, data: DeleteObjectOutput) => {
@@ -40,56 +39,55 @@ export class UserController extends BaseController<User> {
 
   async register(req: Request, res: Response): Promise<Response> {
     const { email, password } = req.body;
-    const existedUser: User = await this._userRepository.getOne(email, 'email');
-    if (existedUser instanceof MongoError) {
-      return UserController.resolveErrorResponse(res, 400, null, existedUser);
+
+    try {
+      const existedUser: User = await this._userRepository.getOne(email, 'email');
+
+      if (existedUser) {
+        return UserController.resolveErrorResponse(res, 400, 'Email exists');
+      }
+
+      const newUser: User = new UserModel({
+        email,
+        password,
+      });
+
+      const result: User = await this._userRepository.create(newUser);
+      const savedUser: User = await result.save();
+
+      const token: string = sign({ user: savedUser }, coreConfig.JWT.secret, { expiresIn: '7d' });
+      return UserController.resolveResponse(res, 'Registration successfully', { token });
+    } catch (e) {
+      return UserController.resolveErrorResponse(res, 400, null, e);
     }
-
-    const newUser: User = new UserModel({
-      email,
-      password
-    });
-
-    const result: User = await this._userRepository.create(newUser);
-
-    if (result instanceof MongoError) {
-      return UserController.resolveErrorResponse(res, 500, null, result);
-    }
-
-    const savedUser: User = await result.save();
-    const token: string = sign({ user: savedUser }, coreConfig.JWT.secret, { expiresIn: '7d' });
-
-    return UserController.resolveResponse(res, 'Registration successfully', { token });
   }
 
   async login(req: Request, res: Response): Promise<Response> {
     const { email, password } = req.body;
 
-    const fetched: User = await this._userRepository.getOne(email, 'email');
+    try {
+      const fetched: User = await this._userRepository.getOne(email, 'email');
 
-    if (fetched instanceof MongoError) {
-      return UserController.resolveErrorResponse(res, 500, null, fetched);
+      if (!fetched || fetched === null) {
+        return UserController.resolveErrorResponse(res, 400, 'Invalid Email/Password Combination');
+      }
+
+      const isMatched: boolean = await compare(password, fetched.password);
+
+      if (!isMatched) {
+        return UserController.resolveErrorResponse(res, 400, 'Invalid Email/Password Combination');
+      }
+
+      fetched.lastLogin = new Date();
+      const result: User = await this._userRepository.update(fetched._id, fetched);
+      delete result.password;
+
+      const token: string = sign({ user: result }, coreConfig.JWT.secret, { expiresIn: '7d' });
+
+      return UserController.resolveResponse(res, 'Login Successful', { token });
+    } catch (e) {
+      return UserController.resolveErrorResponse(res, 500, null, e);
     }
-
-    if (!fetched || fetched === null) {
-      return UserController.resolveErrorResponse(res, 400, 'Invalid Email/Password Combination');
-    }
-
-    const isMatched: boolean = await compare(password, fetched.password);
-
-    if (!isMatched) {
-      return UserController.resolveErrorResponse(res, 400, 'Invalid Email/Password Combination');
-    }
-
-    fetched.lastLogin = new Date();
-    const result: User = await this._userRepository.update(fetched._id, fetched);
-
-    if (result instanceof MongoError) {
-      return UserController.resolveErrorResponse(res, 500, null, result);
-    }
-
-    const token: string = sign({ user: result }, coreConfig.JWT.secret, { expiresIn: '7d' });
-    return UserController.resolveResponse(res, 'Login Successful', { token });
   }
 
   async getProfile(req: Request, res: Response): Promise<Response> {
@@ -101,59 +99,59 @@ export class UserController extends BaseController<User> {
         displayName: currentUser.displayName,
         imageURL: currentUser.imageURL,
         created: currentUser.created,
-        lastLogin: currentUser.lastLogin
-      }
+        lastLogin: currentUser.lastLogin,
+      },
     });
   }
 
   async uploadProfile(req: Request, res: Response): Promise<Response> {
-    const currentUser: User = await this._userRepository.getById((req['user'] as User)._id);
-    const { displayName, password } = req.body;
-    const file = req['file'] as S3File;
+    try {
+      const currentUser: User = await this._userRepository.getById((req['user'] as User)._id);
+      const { displayName, password } = req.body;
+      const file = req['file'] as S3File;
 
-    if (currentUser instanceof MongoError) {
-      return UserController.resolveErrorResponse(res, 500, null, currentUser);
+      if (displayName) {
+        currentUser.displayName = displayName;
+      }
+
+      if (password) {
+        currentUser.password = password;
+      }
+
+      if (file !== undefined) {
+        if (currentUser.hasUploadedImage) {
+          this.deleteProfilePic(currentUser.imageKey);
+        }
+
+        currentUser.imageURL = file.location;
+        currentUser.imageKey = file.key;
+        currentUser.hasUploadedImage = true;
+      }
+
+      await currentUser.save();
+      return UserController.resolveResponse(res, 'Profile updated successfully');
+    } catch (e) {
+      return UserController.resolveErrorResponse(res, 500, null, e);
     }
+  }
 
-    if (displayName) {
-      currentUser.displayName = displayName;
-    }
+  async resetProfilePic(req: Request, res: Response): Promise<Response> {
+    try {
+      const currentUser: User = await this._userRepository.getById((req['user'] as User)._id);
 
-    if (password) {
-      currentUser.password = password;
-    }
-
-    if (file !== undefined) {
       if (currentUser.hasUploadedImage) {
         this.deleteProfilePic(currentUser.imageKey);
       }
 
-      currentUser.imageURL = file.location;
-      currentUser.imageKey = file.key;
-      currentUser.hasUploadedImage = true;
+      currentUser.imageURL = req.body.imageURL;
+      currentUser.imageKey = '';
+      currentUser.hasUploadedImage = false;
+
+      await currentUser.save();
+      return UserController.resolveResponse(res, 'Profile updated successfully');
+    } catch (e) {
+      return UserController.resolveErrorResponse(res, 500, null, e);
     }
-
-    await currentUser.save();
-    return UserController.resolveResponse(res, 'Profile updated successfully');
-  }
-
-  async resetProfilePic(req: Request, res: Response): Promise<Response> {
-    const currentUser: User = await this._userRepository.getById((req['user'] as User)._id);
-
-    if (currentUser instanceof MongoError) {
-      return UserController.resolveErrorResponse(res, 500, null, currentUser);
-    }
-
-    if (currentUser.hasUploadedImage) {
-      this.deleteProfilePic(currentUser.imageKey);
-    }
-
-    currentUser.imageURL = req.body.imageURL;
-    currentUser.imageKey = '';
-    currentUser.hasUploadedImage = false;
-
-    await currentUser.save();
-    return UserController.resolveResponse(res, 'Profile updated successfully');
   }
 }
 
